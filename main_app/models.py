002bf2f0ab37fc0d1dee61fc5f5a327a935f7c95
samtasticc <samantha.henesy@gmail.com>
2025-02-06 00:47:15 -0500

    create create route

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Destination(models.Model):
@@ -8,3 +9,6 @@ class Destination(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('platepick-detail', kwargs={'platepick_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .models import Destination
 
 # Create your views here.
+
+class DestinationCreate(CreateView):
+    model = Destination
+    fields = '__all__'
+    sucess_url = '/platepick/'
